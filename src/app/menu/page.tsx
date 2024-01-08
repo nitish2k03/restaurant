b6ac5ae826e8f6ae2675609e4e4ce1557b8acec3
Nitish <nitish.mahajanhalf8@gmail.com>
2024-01-09 02:50:51 +0530

    Menu Page Done

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { menu } from "~/data";
+import Link from "next/link";
 const MenuPage = () => {
   return (
-    <div className="flex md:flex-row flex-col h-[calc(100vh-4rem)]">
+    <div className="flex md:flex-row flex-col h-[calc(100vh-4rem)] p-4 items-center">
       {menu.map((singleMenu) => (
-        <div key={singleMenu.id} className="back">
-          <div>{singleMenu.title}</div>
-          <div>{singleMenu.desc}</div>
-        </div>
+        <Link
+          href={`menu/${singleMenu.slug}`}
+          key={singleMenu.id}
+          className={`bg-cover h-[50%] p-2 text-${singleMenu.color} flex-1 w-full`}
+          style={{ backgroundImage: `url(${singleMenu.img})` }}
+        >
+          <div className="flex flex-col justify-around  h-[100%] w-[50%]">
+            <div className="font-bold text-4xl">{singleMenu.title}</div>
+            <div>{singleMenu.desc}</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
