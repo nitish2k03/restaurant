c39496f30f4ac0de4d8eede37ec1c33c19861fa6
Nitish <nitish.mahajanhalf8@gmail.com>
2024-01-10 04:20:43 +0530

    Login route created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginPage from "~/components/Login";
+const Login = () => {
+  return (
+    <div className="w-100%">
+      <LoginPage />
+    </div>
+  );
+};
+
+export default Login;
